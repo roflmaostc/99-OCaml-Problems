6339e7e077b1b407cc46a33c269d358d40a8821f
roflmaostc <f.wechsler@tum.de>
2017-12-10 19:53:56 +0100

    61 solved with tail and non-tail recursive version

---
+++ problem_61.ml
@@ -0,0 +1,22 @@
+type 'a binary_tree =
+  | Empty
+  | Node of 'a * 'a binary_tree * 'a binary_tree;;
+
+
+(*straightforward non tail recursive*)
+let rec count_leaves t = match t with 
+  | Empty -> 0
+  | Node (_, l,r ) -> 1+(count_leaves l)+(count_leaves r)
+
+
+type cont = Cont of (int -> cont list ->int)
+
+(*tail recursive version*)
+let rec count_leaves2 t = 
+  let rec aux (t:'a binary_tree) (counter:int) (funs:cont list) = match t with
+    | Empty -> (match funs with
+      | [] -> counter
+      | (Cont h)::ts -> h counter ts)
+    | Node(v,left,right) -> aux left (counter+1) ( (Cont (fun c tail -> aux right c tail))::funs)
+  in
+  aux t 0 []
