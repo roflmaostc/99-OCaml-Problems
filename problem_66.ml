24285b351933393de19143f8a251f5ee677a17f3
roflmaostc <f.wechsler@tum.de>
2017-12-15 15:41:30 +0100

    solved for balanced binary tree

--- problem_66.ml
+++ problem_66.ml
@@ -8,8 +8,8 @@ type 'a binary_tree =
 let left_border t x_off = 
   let rec left_border_to_list t x_off acc = match t with
     | Empty -> acc
-    | Node ((v, xpos, ypos, x_off_new), Empty, right) -> left_border_to_list right (x_off_new + x_off) ((xpos+x_off)::acc)
-    | Node ((v, xpos, ypos, x_off_new), left, _) -> left_border_to_list left (x_off_new+x_off) ((xpos-x_off)::acc)
+    | Node ((v, xpos, ypos, x_off_new), Empty, right) -> left_border_to_list right (x_off+x_off_new) ((xpos+x_off)::acc)
+    | Node ((v, xpos, ypos, x_off_new), left, _) -> left_border_to_list left (x_off -x_off_new) ((xpos+x_off)::acc)
   in
   List.rev (left_border_to_list t x_off [])
 
@@ -19,8 +19,8 @@ let left_border t x_off =
 let right_border t x_off =
   let rec right_border_to_list t x_off acc = match t with
     | Empty -> acc
-    | Node ((v, xpos, ypos, x_off_new), left, Empty) -> right_border_to_list left (x_off_new+x_off) ((xpos-x_off)::acc)
-    | Node ((v, xpos, ypos, x_off_new), _, right) -> right_border_to_list right (x_off_new+x_off) ((xpos+x_off)::acc)
+    | Node ((v, xpos, ypos, x_off_new), left, Empty) -> right_border_to_list left (x_off-x_off_new) ((xpos+x_off)::acc)
+    | Node ((v, xpos, ypos, x_off_new), _, right) -> right_border_to_list right (x_off+x_off_new) ((xpos+x_off)::acc)
   in
   List.rev (right_border_to_list t x_off [])
 
@@ -32,28 +32,44 @@ let rec check_collision left_l right_l = match (left_l,right_l) with
 
 let update_xpos t x_dist = match t with
   | Empty -> Empty
-  | Node((v, xpos, ypos, x_offset_childs), l,r) -> Node((v, xpos+x_dist, ypos, x_offset_childs), l, r)
+  | Node((v, xpos, ypos, x_offset_childs), l,r) -> Node((v, xpos, ypos, x_offset_childs+x_dist), l, r)
 
+
+let rec finalize tree x_offset = match tree with
+  | Empty -> Empty
+  | Node((v,xpos,ypos, x_offset_childs), l, r) -> Node ((v, xpos+x_offset, ypos), finalize l (x_offset-x_offset_childs), finalize r (x_offset+x_offset_childs))
 
 (*Shifts subtree left and right until there is no intersections of the subtrees*)
-let rec find_valid_t left_tree right_tree x_dist = 
-  if check_collision (left_border left_tree (-x_dist)) (right_border right_tree x_dist) then
-    find_valid_t left_tree right_tree (x_dist+1)
+let rec find_valid_t left_tree right_tree x_dist_childs = 
+  if check_collision (right_border left_tree (-x_dist_childs)) (left_border right_tree x_dist_childs) then
+    (Printf.printf "Colission\n"; find_valid_t left_tree right_tree (x_dist_childs+1))
   else 
-    let l = update_xpos left_tree (-x_dist) 
-    in
-    let r = update_xpos right_tree (x_dist)
-    in
-    (x_dist, l, r)
+    (* let l = update_xpos left_tree (-x_dist_childs) *) 
+    (* in *)
+    (* let r = update_xpos right_tree (x_dist_childs) *)
+    (* in *)
+    (x_dist_childs, left_tree, right_tree)
 
-(* let rec layout tree x_off y = match tree with *)
-(*   | Empty -> Empty *)
-(*   | Node(v,left,right) -> *) 
-(*     let xpos = x_off *)
-(*     in *)
-(*     let l_new = layout left x_off (y+1) *)
-(*     in *)
-(*     let r_new = layout right x_off (y+1) *)
-(*     in *)
-    
+let rec layout tree depth = match tree with
+  | Empty -> Empty
+  | Node(v,left,right) -> 
+    let l_new = layout left (depth+1)
+    in
+    let r_new = layout right (depth+1)
+    in
+    let (x_offset_childs, l_new, r_new) = find_valid_t l_new r_new 1
+    in
+    Node((v, 0, depth, x_offset_childs), l_new, r_new)
 
+let rec left_pos t = match t with
+  | Empty -> 1
+  | Node((_,x,_),l,r) -> if l=Empty then x else left_pos l
+
+let rec shift t off = match t with
+  | Empty -> Empty
+  | Node((v,x,y), l,r) -> Node((v,x-off+1,y), shift l off, shift r off)
+
+let layout_binary_tree_3 t =
+  let t = finalize (layout t 1) 0
+  in
+  shift t (left_pos t);;
