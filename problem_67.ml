1a695b354d484fe332e744d4a99db7bfc22c66b6
roflmaostc <f.wechsler@tum.de>
2017-12-16 14:48:15 +0100

    67 solved

--- problem_67.ml
+++ problem_67.ml
@@ -7,3 +7,19 @@ let rec string_of_tree t = match t with
   | Node(x,l,r) -> 
     if l = Empty && r = Empty then (String.make 1 x)
     else (String.make 1 x)^"("^(string_of_tree l)^","^(string_of_tree r)^")"
+
+
+
+let tree_of_string str = 
+  let rec aux str pos = 
+    if pos>=String.length str || str.[pos]= ',' || str.[pos] = ')' then
+      (Empty, (pos))
+    else if pos +1 < String.length str && str.[pos+1]='(' then 
+      let l, pos_new = aux str (pos+2)
+      in
+      let r,pos_new = aux str (pos_new+1)
+      in
+      Node(str.[pos], l, r), (pos_new+1)
+    else (Node(str.[pos], Empty, Empty), pos+1)
+  in
+  aux str 0
