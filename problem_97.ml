932fbb26b417abbc47673d4d9f2af7c897957276
roflmaostc <f.wechsler@tum.de>
2018-01-06 18:08:05 +0100

    97 solved

---
+++ problem_97.ml
@@ -0,0 +1,60 @@
+exception Complete
+
+(* Checks wether a element at (x,y) violates any of the rules*)
+let violation s x y ele =
+  let rec vio_horizontal c =
+    if c >= 9 then false
+    else if s.(y).(c)=ele then true
+    else vio_horizontal (c+1)
+  in
+  let rec vio_vertical c =
+    if c >= 9 then false
+    else if s.(c).(x)=ele then true
+    else vio_vertical (c+1)
+  in
+  let base_x = 3*(x/3)
+  in
+  let base_y = 3*(y/3)
+  in
+  let rec vio_box c =
+    if c >= 9 then false
+    else if s.(base_y+c/3).(base_x+(c mod 3))=ele then true
+    else vio_box (c+1)
+  in
+  vio_horizontal 0 || vio_vertical 0 || vio_box 0
+
+let rec next_field (x,y) board = 
+  if x=8 then
+    if y=8 then
+      raise Complete
+    else 
+    (if board.(y+1).(0) = 0 then
+      (0,y+1)
+     else 
+      next_field (0,y+1) board
+    )
+  else
+    (if board.(y).(x+1) = 0 then
+      (x+1,y)
+     else 
+      next_field (x+1,y) board
+    )
+
+let solve board = 
+  let rec aux changed (x,y) number = 
+    if number=10 then
+      let() = board.(y).(x) <- 0
+      in
+      let x,y, number = List.hd changed
+      in
+      aux (List.tl changed) (x,y) (number+1)
+    else
+      if violation board x y number then
+        aux changed (x,y) (number+1)
+      else
+        let() = board.(y).(x) <- number
+        in
+        aux ((x,y,number)::changed) (next_field (x,y) board) 1
+  in 
+  try aux [] (next_field (-1,0) board) 1 
+  with Complete -> 0
