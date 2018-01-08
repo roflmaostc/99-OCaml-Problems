03770bb2523001e8b3dc0ed151d7b32e19341feb
roflmaostc <f.wechsler@tum.de>
2018-01-08 23:37:27 +0100

    improvements for larger number >30 elements. String improvement

--- problem_94.ml
+++ problem_94.ml
@@ -72,13 +72,17 @@ let arithmetic_puzzle l =
     (if (eval (List.rev firstc) (List.rev firstl)) = (eval (List.rev secondc) (List.rev secondl)) then true
     else false) with Division_by_zero -> false
   in
-  let rec solve counter border c = 
-    if counter = border then raise No_Solution
-    else if eval_equation c l = false then
+  let rec solve c = 
+    if eval_equation c l = false then
       let b,c = next_combination c 
       in
-      if b then solve (counter+1) border (snd (next_equal_pos c))
-      else solve (counter+1 ) border c
+      if b then 
+        let b2, c = next_equal_pos c
+        in
+        if b2=false then raise No_Solution
+        else
+            solve c
+      else solve c
     else c 
   in
   let operator_string x = if x=0 then "="
@@ -88,11 +92,16 @@ let arithmetic_puzzle l =
     else if x=4 then "/"
     else failwith "failure in operator string"
   in
-  let rec to_string c l acc = match (c,l) with
+  let rec to_string c l acc b = match (c,l) with
     | ([], [x]) -> acc^(string_of_int x)
-    | (hdc::tlc, hdl::tll) -> to_string tlc tll  ("("^acc^(string_of_int hdl)^")"^operator_string hdc)
+    | (hdc::tlc, hdl::tll) -> 
+      if hdc = 0 then
+        acc^(string_of_int hdl)^"="^(to_string tlc tll  "" false)
+      else
+      to_string tlc tll  ((if b then "(" else "")^acc^(string_of_int hdl)^(if b then ")" else "")^operator_string hdc) true
+    | _ -> failwith "Failure in to_string"
   in
-  let str = to_string (solve 0 ( (int_of_float (4.0**(float_of_int(n-2))))*(n-1)) (List.rev_append (List.rev_map (fun _ -> 1 ) (List.tl (List.tl l))) [0])) l ""
+  let str = to_string (solve  (List.rev_append (List.rev_map (fun _ -> 1 ) (List.tl (List.tl l))) [0])) l "" false
   in
   str
 
