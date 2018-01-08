61e805ded365a4377e52c3d9f7c61c01b1de7ae8
roflmaostc <f.wechsler@tum.de>
2018-01-08 19:22:10 +0100

    first solution, quite long

---
+++ problem_94.ml
@@ -0,0 +1,102 @@
+
+
+exception Solution_Found
+exception No_Solution
+(*Constraint
+ * combis is list of arithmetic operations
+ * 0 is =
+ * 1 is +
+ * 2 is -
+ * 3 is * 
+ * 4 is /
+ * *)
+let arithmetic_puzzle l =
+  let n = List.length l
+  in
+  let next_combination combis =
+    List.fold_right (fun ele (b, acc) ->
+        if ele = 0 then
+          (b, ele::acc)
+        else
+          if b = true then
+            if ele = 4 then
+              (true, 1::acc)
+            else
+              (false, ele+1::acc)
+          else
+            (false, ele::acc)
+      )
+      combis (true, [])
+  in
+  let next_equal_pos combis = 
+    List.fold_right (fun ele (b, acc) -> 
+        if b=true then 
+          (false, 0::acc) 
+        else if ele=0 then
+          (true, 1::acc)
+        else 
+          (false, ele::acc)
+    ) combis (false , [])
+  in
+  let operator x = if x = 1 then (+)
+    else if x = 2 then (-)
+    else if x = 3 then ( * )
+    else if x = 4 then (/)
+    else failwith "no operator to this number "
+  in
+  let rec eval c l = match (c,l) with
+    |([], [x]) -> x
+    | (hdc::tlc, hdl::tll) -> (operator hdc) (eval tlc tll) hdl
+    | _ -> failwith "invalid input"
+  in
+  let split c l = 
+    let rec aux c l b = 
+      match (c,l) with 
+      | ([], [x]) -> ([],[],[], [x])
+      | (hdc::tlc, hdl::tll) -> 
+          let acc_f_c, acc_f_l, acc_s_c, acc_s_l = aux tlc tll (if hdc= 0 then true else b)
+          in
+          if hdc=0
+          then acc_f_c, hdl::acc_f_l, acc_s_c, acc_s_l
+          else
+            if b then acc_f_c, acc_f_l, hdc::acc_s_c, hdl::acc_s_l 
+            else hdc::acc_f_c, hdl::acc_f_l, acc_s_c, acc_s_l
+      | _ -> failwith "Error in split"
+    in
+    aux c l false
+  in
+  let eval_equation c l = 
+    let firstc, firstl, secondc, secondl = split c l
+    in
+    try 
+    (if (eval (List.rev firstc) (List.rev firstl)) = (eval (List.rev secondc) (List.rev secondl)) then raise Solution_Found
+    else false) with Division_by_zero -> false
+  in
+  let rec solve counter border c = 
+    try (
+    if counter = border then raise No_Solution
+    else if eval_equation c l = false then
+      let b,c = next_combination c 
+      in
+      if b then solve (counter+1) border (snd (next_equal_pos c))
+      else solve (counter+1 ) border c
+    else failwith "blub") with Solution_Found -> c
+  in
+  let operator_string x = if x=0 then "="
+    else if x=1 then "+"
+    else if x=2 then "-"
+    else if x=3 then "*"
+    else if x=4 then "/"
+    else failwith "failure in operator string"
+  in
+  let rec to_string c l acc = match (c,l) with
+    | ([], [x]) -> acc^(string_of_int x)
+    | (hdc::tlc, hdl::tll) -> to_string tlc tll  ("("^acc^(string_of_int hdl)^")"^operator_string hdc)
+  in
+  let str = to_string (solve 0 ( (int_of_float (4.0**(float_of_int(n-2))))*(n-1)) (List.rev_append (List.rev_map (fun _ -> 1 ) (List.tl (List.tl l))) [0])) l ""
+  in
+  str
+
+
+
+
